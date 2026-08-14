96c184676ee35cbb7b1aec75b1e54651b6d29c60
aron-bs <Aron.BuckleySmith@dsit.gov.uk>
2026-08-14 15:50:34 +0100

    FLEX-000 fix: temp alarms rename revert

--- platform/infra/flex/src/utils/create-service-gateway.ts
+++ platform/infra/flex/src/utils/create-service-gateway.ts
@@ -44,8 +44,7 @@ export function createServiceGateway(
 ) {
   const serviceGateway = createFunction(scope, {
     access: config.access,
-    // TODO: Revert to original name
-    id: `${toPascalCase(config.name)}ServiceGateway${config.name === "dvla" ? "-temp" : ""}`,
+    id: `${toPascalCase(config.name)}ServiceGateway`,
     enableDefaultAlarms: config.function?.enableDefaultAlarms,
     securityGroups,
     functionProps: {
